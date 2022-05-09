d213fcb25031184eaaa47a6a67659d3399f96ddd
Marc-Olivier <sossou_m@etna-alternance.net>
2022-05-09 16:48:31 +0200

    [9 Mai] - Ajout du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
         <div className='items-center mx-auto'>
             <div name="texte_footer" className='font-light text-white text-center text-2xl'>
                 <p>
-                    Ce site a été réalisé avec React JS et Tailwind CSS. 
-                        <span className='hover:text-slate-400 duration-200'> 
+                    Ce site a été réalisé avec React JS et Tailwind CSS.  
+                        <span className='ml-2 hover:text-slate-400 duration-200'> 
                             <a href='https://github.com/MarcOlivier91/portfolio-personnel/tree/master' 
                             target="_blank" rel="noopener noreferrer">
                                 Voir le code source.
